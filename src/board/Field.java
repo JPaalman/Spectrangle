250e70d3a21a693a6bce14f2cd73b56a723723b0
Jaïr Paalman <j.paalman@outlook.com>
2019-01-29 13:32:57 +0100

    added Field

---
+++ src/board/Field.java
@@ -0,0 +1,36 @@
+package group92.spectrangle.board;
+
+import group92.spectrangle.exceptions.MoveException;
+import group92.spectrangle.view.GUI;
+
+import java.util.Observable;
+
+public class Field extends Observable {
+
+    // TODO improve Observer
+
+    private int multiplier;
+
+    private Tile tile;
+
+    public Field(int multiplier) {
+        this.multiplier = multiplier;
+        addObserver(GUI.get());
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public int place(Tile tile) throws MoveException {
+        if (this.tile == null) {
+            this.tile = tile;
+            setChanged();
+            notifyObservers();
+            return tile.getMultiplier() * multiplier;
+        } else {
+            throw new MoveException("field is not empty");
+        }
+    }
+
+}
