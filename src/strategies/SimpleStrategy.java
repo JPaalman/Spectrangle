d99fb4ce4af6d48cf3692e4cccc0561ec862de95
rHendriks <r.hendriks@student.utwente.nl>
2019-01-30 14:13:49 +0100

    .

--- src/strategies/SimpleStrategy.java
+++ src/strategies/SimpleStrategy.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class SimpleStrategy extends RandomStrategy {
 
+    @Override
     public Move getMove(ArrayList<Move> moves) {
         ArrayList<Move> temp = new ArrayList<>();
         for (Move move : moves) {
